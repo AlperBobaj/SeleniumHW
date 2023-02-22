19117449ba7761f54741f2873dd98a57361e9f9a
alper bobaj <alperbobaj@alpers-mbp-2.myfiosgateway.com>
2023-02-22 18:19:23 -0500

    Hw1

---
+++ src/HomeWork01/Task01.java
@@ -0,0 +1,24 @@
+package HomeWork01;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Task01 {
+    public static void main(String[] args) {
+        /**
+         * 1. launch the browser
+         * 2. navigate to amazon website
+         * 3. print out the title and the url in the console
+         * 4. close the browser
+         */
+
+
+        System.setProperty("webdriver.chrome.driver","Drivers/chromedriver");
+
+       WebDriver driver=new ChromeDriver();
+       driver.navigate().to("https://www.amazon.com");
+        System.out.println(driver.getTitle());
+        driver.close();
+
+    }
+}
